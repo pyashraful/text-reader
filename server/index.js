ee38840b517bb8ba28bb7b5ad6ca54194573ec4b
Py-Ashraful <ashrafulislam180@gmail.com>
2022-06-23 01:41:38 +0600

    signin route:

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
-
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import http from "http";
+import signinRouter from "./router/signinRouter.js";
+import signupRouter from "./router/signupRouter.js";
+
 const app = express();
 dotenv.config();
 
@@ -17,6 +19,10 @@ mongoose
   })
   .then(console.log("database connection successful!"))
   .catch((err) => console.log(err));
+
+//Router
+app.use("/signin", signinRouter);
+app.use("/signup", signupRouter);
 
 server.listen(process.env.PORT || 5000, () => {
   console.log(`app listening to port ${process.env.PORT}`);
