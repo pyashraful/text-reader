1b7be2b04a64cf8d5da38437b3ed9819b817c767
Py-Ashraful <ashrafulislam180@gmail.com>
2022-06-22 23:23:29 +0600

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import http from "http";
@@ -8,6 +9,14 @@ const app = express();
 dotenv.config();
 
 const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("database connection successful!"))
+  .catch((err) => console.log(err));
 
 server.listen(process.env.PORT || 5000, () => {
   console.log(`app listening to port ${process.env.PORT}`);
