87bd5cadcfe706d6087c19ed3f5e3292cc5c20cb
Py-Ashraful <ashrafulislam180@gmail.com>
2022-06-25 23:53:01 +0600

    user model

---
+++ server/model/user.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const users = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const People = mongoose.model("Users", users);
+
+module.exports = People;
