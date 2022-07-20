effd35af88d9c25740dbb5807a6ecdb36a09b559
Py-Ashraful <ashrafulislam180@gmail.com>
2022-07-20 23:56:51 +0600

    frontend auth

---
+++ client/src/feactures/auth/authSlice.js
@@ -0,0 +1,63 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+const initialState = {
+  user: null,
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  massage: "",
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkApi) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const massage =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkApi.rejectWithValue({ massage });
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isError = false;
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.massage = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.massage = action.payload.massage;
+        state.user = null;
+      });
+  },
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;
