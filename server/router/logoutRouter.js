f6bb4cf1bd60c1b7b4847137cd2f3a7a575e3457
Py-Ashraful <ashrafulislam180@gmail.com>
2022-10-13 23:01:04 +0600

    router deleted

--- server/router/logoutRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import logout from "../controller/logout.js";
-
-const router = express.Router();
-
-router.post("/", logout);
-
-export default router;
--- server/router/signinRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import signin from "../controller/signin.js";
-
-const router = express.Router();
-
-router.post("/", signin);
-
-export default router;
